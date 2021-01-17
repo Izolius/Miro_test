979b3a01987ce16ffa347325cee71a67fd94ee1f
Izolius <andreev-09@yandex.ru>
2021-01-17 11:59:13 +0500

    test

--- src/test/java/com/mirotest/mirotest_server/MirotestServerApplicationTests.java
+++ src/test/java/com/mirotest/mirotest_server/MirotestServerApplicationTests.java
@@ -2,12 +2,14 @@ package com.mirotest.mirotest_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class MirotestServerApplicationTests {
 
     @Test
     void contextLoads() {
+        Assert.isTrue(false);
     }
 
 }
